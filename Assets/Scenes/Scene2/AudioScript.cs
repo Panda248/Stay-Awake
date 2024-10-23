832de67eaf2dce06057c1cf5331801b4ba60b3be
Panda248 <p4nda248@gmail.com>
2024-10-23 17:48:13 -0400

    audio distortion and camera bob

--- Assets/Scenes/Scene2/AudioScript.cs
+++ Assets/Scenes/Scene2/AudioScript.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class AudioScript : MonoBehaviour
 {
     [SerializeField]
     GamePlayLoop GamePlayLoop;
+
+    AudioSource audioComp;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioComp = GetComponent<AudioSource>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        audioComp.pitch = 1 - GamePlayLoop.GetSleepyValue();
     }
 }
