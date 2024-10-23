0c22806e93eb9d171bf027b8464c5e68bc66d7d1
Panda248 <p4nda248@gmail.com>
2024-10-23 19:52:45 -0400

    camera sidebob

--- Assets/Scenes/Scene2/CameraScript.cs
+++ Assets/Scenes/Scene2/CameraScript.cs
@@ -7,7 +7,12 @@ public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField]
     GamePlayLoop GamePlayLoop;
-    float amplitude = 0.01f;
+    [SerializeField, Range(0f,0.01f)]
+    float xAmplitude = 0.01f;
+    [SerializeField, Range(0f, 1f)]
+    float xAmplitudeGrowthRate = 0.5f;
+    [SerializeField, Range(0f, 0.001f)]
+    float yAmplitude = 0.001f;
     float timer = 0;
     // Start is called before the first frame update
     void Start()
@@ -19,7 +24,8 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         timer+= Time.unscaledDeltaTime;
-        Quaternion deltaRotate = Quaternion.Euler(Mathf.Exp(GamePlayLoop.GetSleepyValue())*amplitude*Mathf.Cos(timer), 0, 0);
+        float deltaX = Mathf.Exp(GamePlayLoop.GetSleepyValue() * xAmplitudeGrowthRate) * xAmplitude * Mathf.Cos(timer);
+        Quaternion deltaRotate = Quaternion.Euler(deltaX, yAmplitude * Mathf.Sin(timer/5), 0);
 
         transform.Rotate(deltaRotate.eulerAngles);
     }
