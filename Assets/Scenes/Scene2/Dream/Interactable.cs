33b7b7f738edc61f3e9bbe0728e5f63011a465d3
Panda248 <p4nda248@gmail.com>
2024-12-04 23:42:05 -0500

    done

--- Assets/Scenes/Scene2/Dream/Interactable.cs
+++ Assets/Scenes/Scene2/Dream/Interactable.cs
@@ -6,11 +6,13 @@ public class Interactable : MonoBehaviour
 {
     ParticleSystem particles;
     AudioSource audioSource;
+    MeshRenderer cube;
     // Start is called before the first frame update
     void Start()
     {
         particles = GetComponentInChildren<ParticleSystem>();
         audioSource = GetComponent<AudioSource>();
+        cube = transform.GetChild(1).GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
@@ -24,7 +26,20 @@ public class Interactable : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             particles.Play();
+            if (Random.Range(0f, 1f) < 0.1f) 
+            { 
             audioSource.Play();
+            }
+            cube.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            particles.Stop();
+            cube.enabled = false;
         }
     }
 }
